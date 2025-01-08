f62d37e2e104260c2b38d947cc9f0f5433226bb0
VishalNeosoft <vishal.prajapati@neosoftmail.com>
2025-01-08 17:16:54 +0530

    create a function to send an email

--- app/utils/rabbitmq_utils.py
+++ app/utils/rabbitmq_utils.py
@@ -1,3 +1,7 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import pika
 
 from app.config import settings
@@ -25,3 +29,29 @@ def create_channel(connection):
     except Exception as e:
         print(f"Error creating RabbitMQ channel: {e}")
         raise
+
+
+def send_email(to_email: str, subject: str, body: str):
+    # Create the email message
+    msg = MIMEMultipart()
+    msg["From"] = settings.FROM_EMAIL
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    # Attach the email body
+    msg.attach(MIMEText(body, "plain"))
+
+    # Connect to the Mailtrap SMTP server
+    try:
+        with smtplib.SMTP(
+            settings.MAILTRAP_SMTP_HOST, settings.MAILTRAP_SMTP_PORT
+        ) as server:
+            server.starttls()  # Secure the connection
+            server.login(
+                settings.MAILTRAP_SMTP_USER, settings.MAILTRAP_SMTP_PASSWORD
+            )  # Login to your Mailtrap account
+            text = msg.as_string()
+            server.sendmail(settings.FROM_EMAIL, to_email, text)  # Send the email
+            print(f"Email sent to {to_email}!")
+    except Exception as e:
+        print(f"Error sending email: {e}")
